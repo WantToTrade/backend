e001db859baa17c4ba4f1a63fa3f58a1e47e9a05
Matej Stuchlik <mstuchli@redhat.com>
2015-11-24 16:18:46 +0100

    Add google map and list of offers to front page

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, jsonify, Response, request
 from datetime import datetime
 import json
 
+from flask import Flask, jsonify, Response, request, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.googlemaps import GoogleMaps, Map
+
+
 app = Flask(__name__)
+Bootstrap(app)
+GoogleMaps(app)
 
 offers = [{"currency-from": "CZK",
            "currency-to": "EUR",
            "user-id": 1,
            "range": (250, 500),
-           "date_until": (2015, 12, 15, 14, 6, 40, 155462)}, 
+           "date_until": (2015, 12, 15, 14, 6, 40, 155462),
+           "location": (49.22573, 16.58205)}, 
           {"currency-from": "FOO",
            "currency-to": "BAR",
            "user-id": 2,
            "range": (1000, 1e6),
-           "date_until": (2016, 11, 15, 14, 6, 40, 155462)}, 
+           "date_until": (2016, 11, 15, 14, 6, 40, 155462), 
+           "location": (49.22563, 16.58215)}, 
            ]
 
 users = [{"user-id": 1,
           "name": "Foo Bar"},
          {"user-id": 2,
           "name": "Miro Stuchlik"}]
+
+@app.route('/', methods=['GET'])
+def index():
+    markers = [offer['location'] for offer in offers]
+    gmap = Map(identifier="view-side",
+               lat=49.22573,
+               lng=16.58205,
+               markers=markers,
+               cls="col-md-8")
+               #style="height:100%;width:100%;")
+    return render_template("index.html", gmap=gmap, offers=offers)
 
 @app.route('/api/v1/offers', methods=['GET'])
 def get_offers():
