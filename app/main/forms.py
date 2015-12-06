356103aac012797bc45ec62ebd5060238a89244f
Matej Stuchlik <mstuchli@redhat.com>
2015-12-06 22:02:13 +0100

    Use HTML5 fields

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,16 @@
 from flask.ext.wtf import Form
-from wtforms.ext.sqlalchemy.orm import model_form
+from wtforms.fields.html5 import (IntegerField,
+                                  DateField)
+from wtforms import SelectField, FloatField
 
-from ..models import Offer
+from ..models import Offer, CURRENCIES
 
-OfferForm = model_form(Offer, base_class=Form, exclude=['owner', 'owner_id'])
+CURRENCIES = [(c, c) for c in CURRENCIES]
+
+class OfferForm(Form):
+    currency_from = SelectField('From currency', choices=CURRENCIES)
+    currency_to = SelectField('To currency', choices=CURRENCIES)
+    amount = IntegerField('Amount')
+    expires = DateField('Expiration date')
+    latitude = FloatField('Latitude', default=49.22573)
+    longitude = FloatField('Longitude', default=16.58205)
